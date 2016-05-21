26f6902261338c6195703c06a1bbaaa3078cda1b
Alejandro Ferrera <aleferrera@hotmail.com>
2016-05-21 14:42:51 -0600

    setup logic for game

---
+++ GameTest/SpecFlowFeatureGameSteps.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Game;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace GameTest
+{
+    [Binding]
+    public class SpecFlowFeatureGameSteps
+    {
+        
+    }
+}
