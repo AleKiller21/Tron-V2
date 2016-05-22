217d2fb501c46f24e2c40512582d3898c1f13ace
Alejandro Ferrera <aleferrera@hotmail.com>
2016-05-21 18:48:18 -0600

    Add result property

--- Game/Game.cs
+++ Game/Game.cs
@@ -9,6 +9,7 @@ namespace Game
     public class Game
     {
         public Player[][] Matrix { get; set; }
+        public string Result { get; set; }
         public void Setup(GameOptions gameData)
         {
             for (int row = 0; row < gameData.Rows; row++)
